13517250a1c91469f554e0afaa8ccb14c5ebf9f5
programefacil <programefacil@Gregorys-MacBook-Air.local>
2017-10-18 21:06:04 +0100

    Templates loops e ifs

--- Django/meuProjeto/meuProjeto/settings.py
+++ Django/meuProjeto/meuProjeto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'meuProjeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['meus_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Django/meuProjeto/meuProjeto/views.py
+++ Django/meuProjeto/meuProjeto/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Ola Mundo')
+    sexo = 'm'
+    nome = 'Alfredo'
+    lista = [
+        {'nome': 'Pedro', 'sexo': 'm'},
+        {'nome': 'Maria', 'sexo': 'f'},
+        {'nome': 'Joaquina', 'sexo': 'f'},
+        {'nome': 'Joao', 'sexo': 'm'},
+    ]
+    data = {'lista': lista, 'sexo': sexo, 'nome': nome }
+    return render(request, 'index.html', data)
